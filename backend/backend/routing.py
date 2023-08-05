47662ac50053845d21d31552bb7b848fd1e9c534
MichalPrzyl <michal.przyl@gmail.com>
2023-08-05 17:41:42 +0200

    Removed comments, whitespaces etc. Readabilty

--- backend/backend/routing.py
+++ backend/backend/routing.py
@@ -1,15 +1,8 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
-# import app.routing
 from django.urls import re_path
 from app.consumers import TextRoomConsumer
+
+
 websocket_urlpatterns = [
-    re_path(r'^ws/(?P<room_name_hehe>[^/]+)/$', TextRoomConsumer.as_asgi()),
+    re_path(r'^ws/(?P<room_name>[^/]+)/$', TextRoomConsumer.as_asgi()),  # 'room_name' can be accessed from 'consumers.py' classes in self.scope['kwargs'] I belive
 ]
-# the websocket will open at 127.0.0.1:8000/ws/<room_name>
-# application = ProtocolTypeRouter({
-#     'websocket':
-#         URLRouter(
-#             websocket_urlpatterns
-#         )
-#     ,
-# })
